fb23a6b5f559ab4309dde1ed1a1559c008c88cf7
fairuzathrz <fayizpdp2008@gmai.com>
2026-05-28 15:23:03 +0700

    Pembuatan fungsi display dan inisialisasi posisi tracker

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -72,4 +72,16 @@ public:
         }
     }
 
-   
+    void display()
+    {
+        int FRONT_Position = FRONT;
+        int REAR_Position = REAR;
+
+        // cek apakah antrian kosong
+        if (FRONT_Position == -1)
+        {
+            cout << "\nQueue is empty\n";
+            return;
+        }
+
+       
